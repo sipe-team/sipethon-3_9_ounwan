09007d47649c61654dc89dc49103e6fe504701d7
SEMIN-97 <jsemin.1021@gmail.com>
2025-01-18 21:51:47 +0900

    style: 배경색 적용

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ function RootComponent() {
   return (
     <React.Fragment>
       <main className="mx-auto flex max-h-[840px] w-full justify-center">
-        <div className="flex h-screen w-[500px] max-w-[500px] flex-col items-center justify-between">
+        <div className="flex h-screen w-[500px] max-w-[500px] flex-col items-center justify-between bg-white">
           <Outlet />
         </div>
       </main>
