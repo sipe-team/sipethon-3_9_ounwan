9e60efd84f87d5e2e5c4dbe8fba317547155e410
SEMIN-97 <jsemin.1021@gmail.com>
2025-01-18 19:02:12 +0900

    feat: 결과 페이지 API 연결

--- src/routes/result/$id.lazy.tsx
+++ src/routes/result/$id.lazy.tsx
@@ -1,14 +1,52 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 
 import { DecoratedBox } from '../../components/DecoratedBox.tsx';
+import { FortuneData } from '../../feature/form/form-response-schema.ts';
+import { useMutation } from '@tanstack/react-query';
+import { axiosClient } from '../../feature/axios/axios-client.ts';
+import { useEffect, useState } from 'react';
 
 export const Route = createLazyFileRoute('/result/$id')({
   component: Result,
 });
 
 function Result() {
+  const [data, setData] = useState<FortuneData | null>(null);
+  const [totalScore, setTotalScore] = useState<number>(0);
   const { id } = Route.useParams();
-  console.log(id);
+  const { mutateAsync: getFortuneData, isPending } = useMutation({
+    mutationFn: async (id: string) => {
+      const response = await axiosClient.post<FortuneData>(`results/${id}`);
+      return response.data;
+    },
+    onError: () => {
+      alert('운세 정보 조회에 실패했습니다. 다시 시도해주세요.');
+    },
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (id) {
+        try {
+          const res = await getFortuneData(id);
+          setData(res);
+          setTotalScore(
+            Math.floor(
+              (res.health_score +
+                res.job_score +
+                res.love_score +
+                res.money_score) /
+                4
+            )
+          );
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [id]);
 
   return (
     <section className="flex h-full w-full flex-col">
@@ -19,49 +57,49 @@ function Result() {
         <DecoratedBox type="short">
           <div className="flex flex-col items-center gap-3 py-6 text-center text-[#363E76]">
             <p className="text-2xl">
-              <span className="font-semibold">홍길동</span> 님의 <br /> 2025년
-              운세 총 점수
+              <span className="font-semibold">{data?.username}</span> 님의{' '}
+              <br /> 2025년 운세 총 점수
             </p>
-            <p className="text-5xl font-bold">85점</p>
+            <p className="text-5xl font-bold">{totalScore}점</p>
           </div>
         </DecoratedBox>
         <section>
           <ul className="mx-auto mt-10 flex max-w-[400px] flex-wrap justify-between gap-8">
             <li className="max-w-[180px] flex-[30%]">
               <div className="relative">
-                <img src="/images/snake_love.png" alt="" />
-                <span className="absolute left-[34%] top-[19%] flex h-[66%] w-[53%] items-center justify-center text-2xl">
-                  54점
+                <img src="/images/snake_health.png" alt="" />
+                <span className="absolute left-[34%] top-[19%] flex h-[66%] w-[53%] items-center justify-center text-2xl text-[#7DBEFF]">
+                  {data?.health_score}점
                 </span>
               </div>
               <p className="ml-auto w-[80%] pt-1 text-center text-xl">건강운</p>
             </li>
             <li className="max-w-[180px] flex-[30%]">
               <div className="relative">
-                <img src="/images/snake_love.png" alt="" />
-                <span className="absolute left-[34%] top-[19%] flex h-[66%] w-[53%] items-center justify-center text-2xl">
-                  54점
+                <img src="/images/snake_money.png" alt="" />
+                <span className="absolute left-[34%] top-[19%] flex h-[66%] w-[53%] items-center justify-center text-2xl text-[#64CAE2]">
+                  {data?.money_score}점
                 </span>
               </div>
-              <p className="ml-auto w-[80%] pt-1 text-center text-xl">건강운</p>
+              <p className="ml-auto w-[80%] pt-1 text-center text-xl">재물운</p>
             </li>
             <li className="max-w-[180px] flex-[30%]">
               <div className="relative">
                 <img src="/images/snake_love.png" alt="" />
-                <span className="absolute left-[34%] top-[19%] flex h-[66%] w-[53%] items-center justify-center text-2xl">
-                  54점
+                <span className="absolute left-[34%] top-[19%] flex h-[66%] w-[53%] items-center justify-center text-2xl text-[#809BFF]">
+                  {data?.love_score}점
                 </span>
               </div>
-              <p className="ml-auto w-[80%] pt-1 text-center text-xl">건강운</p>
+              <p className="ml-auto w-[80%] pt-1 text-center text-xl">애정운</p>
             </li>
             <li className="max-w-[180px] flex-[30%]">
               <div className="relative">
-                <img src="/images/snake_love.png" alt="" />
-                <span className="absolute left-[34%] top-[19%] flex h-[66%] w-[53%] items-center justify-center text-2xl">
-                  54점
+                <img src="/images/snake_job.png" alt="" />
+                <span className="absolute left-[34%] top-[19%] flex h-[66%] w-[53%] items-center justify-center text-2xl text-[#3DA9F2]">
+                  {data?.job_score}점
                 </span>
               </div>
-              <p className="ml-auto w-[80%] pt-1 text-center text-xl">건강운</p>
+              <p className="ml-auto w-[80%] pt-1 text-center text-xl">직업운</p>
             </li>
           </ul>
         </section>
