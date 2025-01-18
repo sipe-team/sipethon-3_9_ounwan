da25da44b869130dfa0f386c760e1ced8513673a
jasong <jasong@student.42seoul.kr>
2025-01-18 10:53:25 +0900

    fix: eslint config 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 import importPlugin from 'eslint-plugin-import';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
@@ -15,7 +15,7 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      ...importPlugin.configs.recommended,
+      importPlugin.flatConfigs.recommended,
       prettierConfig,
     ],
     languageOptions: {
@@ -27,10 +27,8 @@ export default tseslint.config(
       },
     },
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      import: importPlugin,
     },
     rules: {
       // TypeScript
